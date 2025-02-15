bafcc8a573e1d5153468b7a9e2ba6b3fa560ca52
digitpic <jkw5033@gmail.com>
2025-02-15 22:06:04 +0900

    chore: set up project

---
+++ src/test/kotlin/com/dkprint/wayfarer/task/request/WayfarerTaskRequestApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.dkprint.wayfarer.task.request
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WayfarerTaskRequestApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
