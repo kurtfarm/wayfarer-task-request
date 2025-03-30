0e357cbec5abc989e90faf7aab39d706a42bb69e
digitpic <jkw5033@gmail.com>
2025-03-30 16:47:58 +0900

    refactor(s3): async upload, delete

--- src/main/kotlin/com/dkprint/wayfarer/task/request/global/config/S3Config.kt
+++ src/main/kotlin/com/dkprint/wayfarer/task/request/global/config/S3Config.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.s3.presigner.S3Presigner
 
 @Configuration
@@ -22,12 +22,12 @@ class S3Config(
     private val secretKey: String,
 ) {
     @Bean
-    fun s3Client(): S3Client {
+    fun s3AsyncClient(): S3AsyncClient {
         val uri: URI = URI.create(endpoint)
         val credentials = AwsBasicCredentials.create(accessKey, secretKey)
         val provider: StaticCredentialsProvider = StaticCredentialsProvider.create(credentials)
 
-        return S3Client.builder()
+        return S3AsyncClient.builder()
             .endpointOverride(uri)
             .credentialsProvider(provider)
             .region(Region.AP_NORTHEAST_2)
